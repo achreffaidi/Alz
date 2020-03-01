d5635f75a00b70a2a53e34cc9354129cb30d5449
SaidFayache <said.fayache@gmail.com>
2020-03-01 21:00:40 +0100

    fix face recognition UI

--- lib/UI/LiveFaceDetect/FaceDetect.dart
+++ lib/UI/LiveFaceDetect/FaceDetect.dart
@@ -108,13 +108,13 @@ class _FaceDetectState extends State<FaceDetect> {
                         padding: const EdgeInsets.all(10.0),
                         child: Icon(Icons.arrow_back , size: 80, color: Colors.white,),
                       )),
-                  Text("FaceRecognition"  ,style: TextStyle(fontSize: _textSizeValue+20 , color: Colors.white , fontWeight: FontWeight.bold),),
+                  Text("Face Recognition"  ,style: TextStyle(fontSize: _textSizeValue+20 , color: Colors.white , fontWeight: FontWeight.bold),),
                   SizedBox(width: 100,)
                 ],
               )
             ),
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.only(top:45.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -122,8 +122,9 @@ class _FaceDetectState extends State<FaceDetect> {
                 children: <Widget>[
 
                   SizedBox(
-                    width: MediaQuery.of(context).size.width-200,
-                    height: MediaQuery.of(context).size.height/2.5,
+
+                    width: MediaQuery.of(context).size.width-300,
+                    height: MediaQuery.of(context).size.height/3,
                     child: CameraMlVision<List<Face>>(
                       key: _scanKey,
                       cameraLensDirection: cameraLensDirection,
@@ -155,6 +156,7 @@ class _FaceDetectState extends State<FaceDetect> {
                   ),
 
                   Container(
+
                     width: MediaQuery.of(context).size.width-200,
                     height: MediaQuery.of(context).size.height/2.5,
                     decoration: BoxDecoration(
@@ -173,7 +175,7 @@ class _FaceDetectState extends State<FaceDetect> {
                         )
                       ],),
 
-                    margin: EdgeInsets.all(0),
+                    margin: EdgeInsets.only(top: 30),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
